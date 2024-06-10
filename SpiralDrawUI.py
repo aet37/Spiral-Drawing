e02fa83c71471d1eb0aff276d2a2f1e1c102e715
Andrew Toader <69984232+aet37@users.noreply.github.com>
2024-06-10 06:17:15 -0700

    commit

--- SpiralDrawUI.py
+++ SpiralDrawUI.py
@@ -226,7 +226,7 @@ class spiralDrawSystem(QtWidgets.QMainWindow):
 
 		# Check to make sure device did not loose connection
 		if self.accelDevice.isConnected:
-			isDownloaded = self.accelDevice.stop_log()
+			isDownloaded = self.accelDevice.stop_log(self.data_save_path + self.trialNameAccelerom)
 		else:
 			print('Connecton lost during recording... Trying to reestablish...')
 			connected = False
