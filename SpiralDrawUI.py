a06e575d8360cd50dd8444b0fa5ddb1d4f0f3102
Andrew Toader <69984232+aet37@users.noreply.github.com>
2024-07-12 10:46:15 -0700

    commit

--- SpiralDrawUI.py
+++ SpiralDrawUI.py
@@ -208,7 +208,7 @@ class spiralDrawSystem(QtWidgets.QMainWindow):
 			self.canvasImprove.axes.plot(x, y2, marker="s", color='r')
 			self.canvasImprove.axes.set_xlabel('Sonication', fontsize=14)
 			self.canvasImprove.axes.set_xlabel('Tremor Reduction (%)', fontsize=14)
-			self.canvasImprove.axes.set_title('Tremor Improvement Intra-Procedure', fontsize=next(fontsizes))
+			self.canvasImprove.axes.set_title('Tremor Improvement Intra-Procedure', fontsize=18)
 			self.canvasImprove.axes.set_xlim([min(x), max(x)])
 			self.canvasImprove.axes.set_ylim([-100, 20])
 			self.canvasImprove.grid()
