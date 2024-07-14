6e12f98e812100ddfd81349fef913045fab81b94
Andrew Toader <69984232+aet37@users.noreply.github.com>
2024-07-13 22:47:36 -0700

    commit

--- SpiralDrawUI.py
+++ SpiralDrawUI.py
@@ -303,9 +303,9 @@ class spiralDrawSystem(QtWidgets.QMainWindow):
 							arr_pts_y.append(int(row[2]))
 
 				# Plot the spirals
-				eval('self.canvasGraph' + str((ii+1)) + '.axes.plot(arr_pts_x, arr_pts_y, color=\'b\')')
-				eval('self.canvasGraph' + str((ii+1)) + '.axes.tight_layout()')
-				eval('self.canvasGraph' + str((ii+1)) + '.axes.draw()')
+				eval('self.canvasGraph' + str((i+1)) + '.axes.plot(arr_pts_x, arr_pts_y, color=\'b\')')
+				eval('self.canvasGraph' + str((i+1)) + '.axes.tight_layout()')
+				eval('self.canvasGraph' + str((i+1)) + '.axes.draw()')
 
 
 
