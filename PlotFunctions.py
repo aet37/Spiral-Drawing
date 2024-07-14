d07dd3dbfd95aecc432b497b203fd78171d98813
Andrew Toader <69984232+aet37@users.noreply.github.com>
2024-07-13 23:00:25 -0700

    commit

--- PlotFunctions.py
+++ PlotFunctions.py
@@ -3,6 +3,6 @@ from matplotlib.figure import Figure
 
 class MplCanvas(FigureCanvas):
 	def __init__(self, parent=None, width=5, height=4, dpi=100):
-		fig = Figure(figsize=(width, height), dpi=dpi)
+		self.fig = Figure(figsize=(width, height), dpi=dpi)
 		self.axes = fig.add_subplot(111)
 		super(MplCanvas, self).__init__(fig)
