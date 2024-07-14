f4d3097a018834519e9657381f8809b46040c755
Andrew Toader <69984232+aet37@users.noreply.github.com>
2024-07-13 23:01:34 -0700

    commit

--- PlotFunctions.py
+++ PlotFunctions.py
@@ -5,4 +5,4 @@ class MplCanvas(FigureCanvas):
 	def __init__(self, parent=None, width=5, height=4, dpi=100):
 		self.fig = Figure(figsize=(width, height), dpi=dpi)
 		self.axes = self.fig.add_subplot(111)
-		super(MplCanvas, self).__init__(fig)
+		super(MplCanvas, self).__init__(self.fig)
