0ab651e56b8de3f81b5693c0d5a9e1dc5a99efe0
Andrew Toader <69984232+aet37@users.noreply.github.com>
2024-07-17 01:31:04 -0700

    commit

--- SpiralDrawUI.py
+++ SpiralDrawUI.py
@@ -688,7 +688,7 @@ class spiralDrawSystem(QtWidgets.QMainWindow):
 			# Load the baseline info
 			with open(self.data_save_path + 'analysis/' + 'accel_baseline_info.csv') as file:
 				for line in file:
-					print(line)
+					print(line.rstrip())
 					if line[0] == 'BaselineIndex':
 						self.accel_baseline = line[1]
 						self.baselineTrialLE.setText(self.accel_psd[self.accel_baseline])
